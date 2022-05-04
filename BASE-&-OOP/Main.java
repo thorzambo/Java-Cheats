7b689b9460d6a53029f104d8a0a9482fec541516
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-04 19:03:46 +0200

    types start

--- BASE-&-OOP/Main.java
+++ BASE-&-OOP/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
-      //METHODS, func inside classes
+      /* 
+      * METHODS
+      */
       public static void main(String[] args) {
             // OOP Language aka everything works within classes and objects. Always ; at the end of any string
-            System.out.println("Test String, Lets fucking finally learn JAVA");
-
+            System.out.println("Test String, Lets finally learn JAVA");
+            // TYPES:
+            int hello = 5;
+            System.out.println(hello);
+            
       }
 }

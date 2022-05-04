b13ddc4a35202ea687108c78682d60e4d9e696c9
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-04 19:00:31 +0200

    Start Java

---
+++ BASE-&-OOP/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+      //METHODS, func inside classes
+      public static void main(String[] args) {
+            // OOP Language aka everything works within classes and objects. Always ; at the end of any string
+            System.out.println("Test String, Lets fucking finally learn JAVA");
+
+      }
+}

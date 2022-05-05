d16b0a119303f3ed488b2943bf831706ea01c156
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-05 18:22:23 +0200

    Arrays JAVA

--- BASE-&-OOP/6-Arrays/Arrays.java
+++ BASE-&-OOP/6-Arrays/Arrays.java
@@ -3,15 +3,27 @@ public class Arrays{
       public static void main(String[] args){
             /* 
             Arrays are collections of elements with a set Size
-                  1. Set the data type your erray needs to be
+                  1. Set the data type your erray needs to be -> int double String boolean char
                   2. set the name of that Array
                   3. new + again type
                   4. Set the size of that Array, how many elements
             */
 
-            int[] newArrInt = new int[5];
-            String[] newArrStr = new String[5];
-            {1, 5, 10, 8, 30}
+            int[] newArrInt = new int[5]; //{1, 5, 10, 8, 30}
+            String[] newArrStr = new String[5]; //{"ciao", "Come", "StAi", "Leo", "Thor"}
+            
+            // Change/add elements to Array
+            newArrInt[0] = 1;
+            newArrInt[1] = 2;
+            newArrInt[2] = 3;
+            newArrInt[3] = 4;
+            newArrInt[4] = 5;
+
+            String[] newArrayStrTest = {"Ciao", "Come", "StAi", "Leo", "Thor"};
+            
+            // Access to Elemnts in Array
+            int x = newArrInt[3];
+            System.out.println(newArrayStrTest);
 
       }
 }

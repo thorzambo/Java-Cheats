79600baf311ab98a731e1c760dfbb17dd2d577b7
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-05 18:41:07 +0200

    For loops Finish 8-

--- BASE-&-OOP/8-ForLoops/Loops.java
+++ BASE-&-OOP/8-ForLoops/Loops.java
@@ -3,18 +3,31 @@ public class Loops {
       public static void main(String[] args){
 
             int x = 100;
+            int[] arr = {1,4,5,7,3,8,2,1,0};
 
-            /* How:
+            /* 
+            ** How **
             Declare a Variable; -> int i = 0;
             Set a condition, like: till i is smaller than 100; i < 100
             lastly, untill that condition is not reached, add 1 to i i++
             */
-            for (int i = 0; i < 1000; i++) {
+
+            for (int i = 0; i < 10000; i++) {
                   System.out.print(i);
             }
+
             for (int i = 0; i < x; i++) {
                   System.out.println(i);
             }
+
+            for (int i = 0; i < arr.length; i++) {
+                  System.out.println(arr[i]);
+                  if (arr[i] == 3) {
+                        System.out.println("Found 3");
+                        return;
+                  }
+            }
+
       }
 
 }

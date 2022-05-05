5efaec5f36ac156448c7ade2eede7412513220fe
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-05 18:32:42 +0200

    Java For loops

---
+++ BASE-&-OOP/8-ForLoops/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+
+      public static void main(String[] args){
+
+            int x = 100;
+
+            /* How:
+            Declare a Variable; -> int i = 0;
+            Set a condition, like: till i is smaller than 100; i < 100
+            lastly, untill that condition is not reached, add 1 to i i++
+            */
+            for (int i = 0; i < 1000; i++) {
+                  System.out.print(i);
+            }
+            for (int i = 0; i < x; i++) {
+                  System.out.println(i);
+            }
+      }
+
+}

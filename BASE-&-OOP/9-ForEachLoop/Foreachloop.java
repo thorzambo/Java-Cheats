2c3dfb6202f30f9df236bdda66ddba0bbfa35b17
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-05 18:46:35 +0200

    Starting For Each Loop

---
+++ BASE-&-OOP/9-ForEachLoop/Foreachloop.java
@@ -0,0 +1,14 @@
+public class Foreachloop{
+      
+      public static void main(String[] args){
+            int[] arr = {1,5,6,4,32,5,88};
+            String[] names = new String[5];
+
+            int count
+            for (int e:arr){
+                  System.out.println(e);
+
+            }     
+
+      }
+}

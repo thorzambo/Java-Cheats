00387c7392436966d337a4313dbe016e457006fb
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-07 08:51:19 +0200

    OuterInner class

--- OOP/5-innerclasses/Main2.java
+++ OOP/5-innerclasses/Main2.java
@@ -2,10 +2,17 @@ public class Main2 {
 
       public static void main(String[] args) {
 
-            OuterClass out = new OuterClass();
-            out.inner();
-            OuterClass.InnerClass in = out.new InnerClass();
+            // 2 ways to access the inner class:
 
+            OuterClass out = new OuterClass();
+            
+            //out.inner(); // we can do this onkly if inner class is public not PRIVATE
+
+            OuterClass.InnerClass in = out.new InnerClass();
             in.display();
+
+            // For inner meth class
+            OuterClass out2 = new OuterClass();
+            out2.inner();
       }
 }
--- OOP/5-innerclasses/OuterClass.java
+++ OOP/5-innerclasses/OuterClass.java
@@ -1,13 +1,35 @@
+/** 
+ * Inner classes are classes inside a class or inside a method
+ */ 
+
+
 public class OuterClass {
 
+      // class inside class
       public class InnerClass {
+
+            // 1 method inside inner class
             public void display() {
                   System.out.println("This is an InnerClass");
             }
       }
 
+      // 1 method in the outer class
+      // here it calls the inner class
       public void inner() {
+            class InnerClassinMeth {
+                  public void displayMeth() {
+                        System.out.println("This is an InnerClass in a Method!");
+                  }
+            }
+
+            // here calls InnerClass 1st
             InnerClass in = new InnerClass();
             in.display();
+
+            // here we call InnerClassinMeth
+            InnerClassinMeth in2 = new InnerClassinMeth();
+            in2.displayMeth();
       }
+
 }

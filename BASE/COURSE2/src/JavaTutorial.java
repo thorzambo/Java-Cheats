1693e0b6135ac9489bed28ddad29d8f967051e2b
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-10 14:06:18 +0200

    New Java

---
+++ BASE/COURSE2/src/JavaTutorial.java
@@ -0,0 +1,13 @@
+public class JavaTutorial {
+
+      public static void main(String[] args){
+            // new line
+            System.out.println("Hello world!");
+            System.out.println("Hello world!");
+            // same line
+            System.out.print("Wollah WOllah Peng");
+            System.out.print("Wollah WOllah Peng");
+
+      }
+
+}

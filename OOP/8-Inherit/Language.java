0edad7e42b2debe12341516cf5fa6b07ef1a9e6a
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-17 07:01:26 +0200

    Exercises

---
+++ OOP/8-Inherit/Language.java
@@ -0,0 +1,25 @@
+public class Language {
+
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+  
+    public Language(String name, int numSpeakers, String regionsSpoken, String wordOrder) {
+      this.name = name;
+      this.numSpeakers = numSpeakers;
+      this.regionsSpoken = regionsSpoken;
+      this.wordOrder = wordOrder;
+    }
+  
+    public void getInfo() {
+      System.out.println(this.name + " is spoken by " + this.numSpeakers + "people mainly in " + this.regionsSpoken + ". The language follows the word order: " + this.wordOrder + ".");
+    }
+  
+  
+    public static void main(String[] args) {
+      Language ita = new Language("Italian", 99999, "Italy, Ch", "subj - verb - obj");
+      System.out.println(ita);
+      ita.getInfo();
+    }
+  }
